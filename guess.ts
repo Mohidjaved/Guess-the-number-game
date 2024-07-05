2f07c05d9788b62c5b2c7492cd9cf0072dbdbec8
Muhammad Mohid <160265312+Mohidjaved@users.noreply.github.com>
2024-07-05 23:28:52 +0500

    Add files via upload

---
+++ guess.ts
@@ -0,0 +1,15 @@
+import inquirer from "inquirer"
+let randomNumber = Math.floor(Math.random()* 10 + 1)
+
+const answer = await inquirer.prompt([
+{
+    name : "userNumber",
+    type : "number",
+    message : "Guess a number between 1 and 10",
+}
+])
+if (answer.userNumber === randomNumber){
+    console.log("Congratulations You guessed it right!")
+}else{
+    console.log("You guessed wrong! The number was " + randomNumber)
+}
